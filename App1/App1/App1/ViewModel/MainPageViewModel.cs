8cf618b128f48209c58686d53e7e30a220a8e3d6
renier.valdes@outlook.com <renier.valdes@outlook.com>
2018-03-08 20:17:22 -0500

    exploring databinding

---
+++ App1/App1/App1/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            Prompt = "Full Name:";
+            Name = "Renier";
+        }
+
+        public string Prompt { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
---
+++ App1/App1/App1/ViewModel/Page2ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1.ViewModel
+{
+    class Page2ViewModel:INotifyPropertyChanged
+    {
+        string labelText = "Hello";
+        public string LabelText
+        {
+            get { return labelText; }
+            set
+            {
+                labelText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Command changedTextCommand;
+        public Command ChangeTextCommand
+        {
+            get
+            {
+                return changedTextCommand ?? (changedTextCommand = new Command(
+                    () =>
+                    {
+                        LabelText = "Goodbye";
+                    }
+                    ));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;        
+        //helpper method for raising property changed event
+        protected void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+    }
+}
